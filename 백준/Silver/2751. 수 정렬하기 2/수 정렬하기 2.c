2c97438c84b1692bfd13ca7421d7595ae8e59c0a
Jimin Yun <127750424+jjmmnd@users.noreply.github.com>
2024-02-04 17:26:46 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 432 ms, Memory: 8928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -1,28 +1,74 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int num[1000000];
-int compare(const void* a, const void* b) {
-	int num1 = *(int*)a;
-	int num2 = *(int*)b;
+void MergeSort(int left, int right);
+void Merge(int left, int mid, int right);
 
-	if (num1 < num2)
-		return -1;
-	if (num1 > num2)
-		return 1;
-	return 0;
-}
+int buffer[1000000];
+int sort[1000000];
 
 int main()
-{	
+{
 	int N;
 	scanf("%d", &N);
-	for (int i = 0; i < N; i++) {
-		scanf("%d", &num[i]);
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &buffer[i]);
 	}
-	qsort(num, N, sizeof(int), compare);
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", num[i]);
+	
+	MergeSort(0, N - 1);
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d\n", buffer[i]);
 	}
-	return 0;
+}
+
+void MergeSort(int left, int right)
+{
+	if (left < right)
+	{
+		int mid = (left + right) / 2;
+		MergeSort(left, mid);
+		MergeSort(mid + 1, right);
+		Merge(left, mid, right);
+	}
+}
+
+void Merge(int left, int mid, int right)
+{
+	int i = left;
+	int j = mid + 1;
+	int cnt = left;
+
+	while (i <= mid && j <= right)
+	{
+		if (buffer[i] <= buffer[j])
+		{
+			sort[cnt++] = buffer[i++];
+		}
+		else
+		{
+			sort[cnt++] = buffer[j++];
+		}
+	}
+
+	if (i > mid)
+	{
+		for (int k = j; k <= right; k++)
+		{
+			sort[cnt++] = buffer[k];
+		}
+	}
+	else
+	{
+		for (int k = i; k <= mid; k++)
+		{
+			sort[cnt++] = buffer[k];
+		}
+	}
+
+	for (int k = left; k <= right; k++)
+	{
+		buffer[k] = sort[k];
+	}
 }
