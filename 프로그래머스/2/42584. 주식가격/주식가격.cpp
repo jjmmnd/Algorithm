42364ab6d68cbe481d14f00800c5daa7e3c14898
Jimin Yun <127750424+jjmmnd@users.noreply.github.com>
2025-10-09 21:53:20 +0900

    [level 2] Title: 주식가격, Time: 29.40 ms, Memory: 26.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    
+    for(int i=0; i<prices.size()-1; i++)
+    {
+        int sec=0;
+        for(int j=i+1; j<prices.size(); j++)
+        {
+            sec++;
+            if(prices[j]<prices[i])
+            {
+                break;
+            }
+        }
+        answer.push_back(sec);
+    }
+    
+    answer.push_back(0);
+    
+    return answer;
+}
