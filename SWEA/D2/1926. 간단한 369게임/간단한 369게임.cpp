8d7d299546c118a1aaea21a79bf5a75600567722
Jimin Yun <127750424+jjmmnd@users.noreply.github.com>
2025-11-01 23:00:06 +0900

    [D2] Title: 간단한 369게임, Time: 5 ms, Memory: 5,832 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.cpp
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+        int N;
+        cin >> N;	// 10이상 1000이하
+
+		for(int i=1; i<=N; i++)
+        {
+            string s = to_string(i);
+            int len = s.length();
+            int clap=0;
+            for(int j=0; j<len; j++)
+            {
+                if(s[j] == '3' || s[j] == '6' || s[j] == '9')
+                    clap++;       
+            }
+            if(clap>0)
+            {
+                for(int k=0; k<clap; k++)
+                    cout << '-';
+                cout << " ";
+            }
+            else
+	            cout << s << " ";
+        }
+        
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
