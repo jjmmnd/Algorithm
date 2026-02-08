f1cac39b5c22a3b1411791d29e69e16f8f116f00
Jimin Yun <127750424+jjmmnd@users.noreply.github.com>
2026-02-08 15:48:02 +0900

    [D4] Title: 햄스터, Time: 417 ms, Memory: 27,656 KB -BaekjoonHub

---
+++ SWEA/D4/8275. 햄스터/햄스터.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+// N <= 6 && X,M <=10
+// 완탐 dfs
+// 다 채워보고 구간합 조건에 맞는다면 햄스터 수가 최대 + 사전순으로 갱신
+
+public class Solution {
+
+    static int N, X, M;
+    static int[][] record;
+    static int[] finalHams;
+    static int[] tmpHams;
+    static boolean isPossible;
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(br.readLine());
+        for(int tc=1; tc<=TC; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            X = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            record = new int[M][3];
+            finalHams = new int[N+1]; // N에 햄스터 수의 합 저장
+            tmpHams = new int[N];
+            isPossible = false;
+
+            for(int m=0; m<M; m++) {
+                st = new StringTokenizer(br.readLine());
+                record[m][0] = Integer.parseInt(st.nextToken()); // l
+                record[m][1] = Integer.parseInt(st.nextToken()); // r
+                record[m][2] = Integer.parseInt(st.nextToken()); // s
+            }
+
+            countHam(0, 0);
+            System.out.print("#"+tc+" ");
+            if(!isPossible) {
+                System.out.println(-1);
+                continue;
+            }
+            for(int n=0; n<N; n++) {
+                System.out.print(finalHams[n]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void countHam(int cnt, int totalHam){
+        if(cnt == N){
+            // 기록과 비교
+            for(int m=0; m<M; m++) {
+                int left = record[m][0];
+                int right = record[m][1];
+                int sum = 0;
+                for(int i=left-1; i<right; i++) {
+                    sum += tmpHams[i];
+                }
+                if(sum != record[m][2]) {
+                    return;
+                }
+            }
+            isPossible = true;
+            // 기록과 전부 일치한 경우 갱신
+
+
+            if(totalHam > finalHams[cnt]) {
+                finalHams = Arrays.copyOf(tmpHams, N+1);
+                finalHams[cnt] = totalHam;
+            } else if(totalHam == finalHams[cnt]) {
+                // 사전순
+                for(int n=0; n<N; n++){
+                    if(tmpHams[n] < finalHams[n]) {
+                        finalHams = Arrays.copyOf(tmpHams, N+1);
+                        finalHams[cnt] = totalHam;
+                        break;
+                    }
+                    else if(tmpHams[n] > finalHams[n]) {
+                        break;
+                    }
+                }
+            }
+            return;
+        }
+
+        for(int x=0; x<=X; x++){
+            tmpHams[cnt] = x;
+            countHam(cnt+1, totalHam+x);
+        }
+    }
+}
