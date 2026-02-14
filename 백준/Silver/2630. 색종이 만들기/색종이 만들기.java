36f866b6b659a0f285b4725968e70c3e19adb61f
Jimin Yun <127750424+jjmmnd@users.noreply.github.com>
2026-02-14 18:05:52 +0900

    [Silver II] Title: 색종이 만들기, Time: 124 ms, Memory: 15668 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n, white, blue;
+    static int[][] grid;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        grid = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        divide(0, 0, n);
+        System.out.println(white);
+        System.out.println(blue);
+
+    }
+
+    public static void divide(int startR, int startC, int size) {
+        // 길이가 1이면 끝
+        if(size == 1){
+            if(grid[startR][startC] == 0) ++white;
+            else ++blue;
+            return;
+        }
+
+        // 전부 같은 값이 아니라면 다시 나누기
+        int sum = 0;
+        for(int i = startR; i<startR+size; i++){
+            for(int j = startC; j<startC+size; j++){
+                sum += grid[i][j];
+            }
+        }
+
+        if(sum == 0) ++white;
+        else if(sum == size*size) ++blue;
+        else{
+            // 4개 탐색 ㄱㄱ
+            divide(startR, startC, size/2);
+            divide(startR, startC+size/2, size/2);
+            divide(startR+size/2, startC, size/2);
+            divide(startR+size/2, startC+size/2, size/2);
+        }
+
+    }
+}
